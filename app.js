b3a742899650db6cce31931525eec047a0138cac
root <root@10.1.0.103>
2018-10-07 13:09:32 +0000

    changed config

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ const mysql = require('mysql');
 
 // First you need to create a connection to the db
 const con = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'admin',
-  database: 'testdb'
+  host: 'mysql.cha2qfcsucw4.us-east-1.rds.amazonaws.com',
+  user: 'shruthi',
+  password: 'Welcomeuser',
+  database: 'testdb',
+  port: '3306'
 });
 
 con.connect((err) => {
